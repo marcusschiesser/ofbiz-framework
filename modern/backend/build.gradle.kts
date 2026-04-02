4f880d917e96f573ca6e21402c5051ab8deba0d9
Marcus Schiesser <mail@marcusschiesser.de>
2026-04-02 16:04:40 +0800

    fix: avoid parity tomcat classloader clashes

--- modern/backend/build.gradle.kts
+++ modern/backend/build.gradle.kts
@@ -30,7 +30,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
